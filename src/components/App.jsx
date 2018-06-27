c9fd60740731a7d5fd6bef6881acdd7fdec13e28
Jimmy McNamara <jmcnamara44@gmail.com>
2018-06-27 11:33:47 -0700

    add image to the App component.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,11 +3,12 @@ import Header from './Header';
 import TicketList from './TicketList';
 import { Switch, Route } from 'react-router-dom';
 import NewTicketForm from './NewTicketForm';
-
+import vegetables from '../assets/images/vegetables.jpg';
 function App() {
   return (
     <div>
       <Header/>
+      <img src={vegetables}/>
       <Switch>
         <Route exact path='/' component={TicketList} />
         <Route path='/newticket' component={NewTicketForm} />
