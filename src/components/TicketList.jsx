5062e2fe2ae642bb6ba663b68abbfe0efdb8c311
Jimmy McNamara <jmcnamara44@gmail.com>
2018-06-27 10:40:02 -0700

    add routing to help queue.

--- src/components/TicketList.jsx
+++ src/components/TicketList.jsx
@@ -10,27 +10,27 @@ var masterTicketList = [
   {
     names: 'Sleater and Kinney',
     location: '4B',
-    issue: 'Fox image not displaying on page, can only see duck?'
+    issue: 'Fox image not disppplaying on page, can only see duck?'
   },
   {
     names: 'Imani & Jacob',
     location: '9F',
     issue: 'Donkey picture not displaying on hover in Zoology app. :('
   }
-];
+]
 
 function TicketList(){
   return (
-      <div>
-        <hr/>
-        {masterTicketList.map((ticket, index) =>
-          <Ticket names={ticket.names}
-            location={ticket.location}
-            issue={ticket.issue}
-            key={index}/>
-        )}
-      </div>
-    );
+    <div>
+      <hr/>
+      {masterTicketList.map((ticket, index) =>
+        <Ticket names={ticket.names}
+          location={ticket.location}
+          issue={ticket.issue}
+          key={index}/>
+      )}
+    </div>
+  )
 }
 
 export default TicketList
