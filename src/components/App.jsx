55fff9946406b8b4f012f446098ee2fc5efae22d
Jimmy McNamara <jmcnamara44@gmail.com>
2018-06-28 11:49:43 -0700

    lift state to dynamically create a ticket list and have it display on the page.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,19 +5,35 @@ import { Switch, Route } from 'react-router-dom';
 import NewTicketControl from './NewTicketControl';
 import vegetables from '../assets/images/vegetables.jpg';
 import Error404 from './Error404';
+class App extends React.Component {
 
-function App() {
-  return (
-    <div>
-      <Header/>
-      <img src={vegetables}/>
-      <Switch>
-        <Route exact path='/' component={TicketList} />
-        <Route path='/newticket' component={NewTicketControl} />
-        <Route component={Error404} />
-      </Switch>
-    </div>
-  );
+  constructor(props) {
+    super(props);
+    this.state = {
+      masterTicketList: []
+    };
+    this.handleAddingNewTicketToList = this.handleAddingNewTicketToList.bind(this);
+  }
+
+  handleAddingNewTicketToList(newTicket){
+    var newMasterTicketList = this.state.masterTicketList.slice();
+    newMasterTicketList.push(newTicket);
+    this.setState({masterTicketList: newMasterTicketList});
+  }
+
+  render(){
+    return (
+      <div>
+        <Header/>
+        <img src={vegetables}/>
+        <Switch>
+          <Route exact path='/' render={()=><TicketList ticketList={this.state.masterTicketList} />} />
+          <Route path='/newticket' render{()=><NewTicketControl onNewTicketCreation={this.handleAddingNewTicketToList} />} />
+          <Route component={Error404} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
